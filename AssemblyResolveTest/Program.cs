d81596781f5c4a8c9879e6c979be8a434f184abe
alexger97 <alexger97@etersoft.ru>
2020-05-19 14:47:19 +0300

    19.05.2020 Modify program

--- AssemblyResolveTest/Program.cs
+++ AssemblyResolveTest/Program.cs
@@ -7,34 +7,76 @@ using System.Threading.Tasks;
 
 namespace AssemblyResolveTest
 {
+    // This is a testing program for debuging one problem with name of resolving assembly
     class Program
     {
 
-        // This is a testing program for debuging one problem with name of resolving assembly
+
         static void Main(string[] args)
         {
             AppDomain currentDomain = AppDomain.CurrentDomain;
 
-            //try to load assembly
             currentDomain.AssemblyResolve += new ResolveEventHandler(MyResolveEventHandler);
 
-            var type = Type.GetType("CommonComponents.Infra.Zero.Uc.Deployment.ClientSoftwareUpdateStrategy, CommonComponents.Infra.Zero.Uc ");
+            //We try to load assembly in different case
+
+            var t0 = Type.GetType("CommonComponents.Infra.Zero.Uc.Deployment.ClientSoftwareUpdateStrategy, CommonComponents.Infra.Zero.Uc, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            Console.WriteLine("*** Type 0 ***");
+
+            var t = Type.GetType("CommonComponents.Infra.Zero.Uc.Deployment.ClientSoftwareUpdateStrategy, CommonComponents.Infra.Zero.Uc");
+            Console.WriteLine("*** Type 1 ***");
+
+            var t2 = Type.GetType("CommonComponents.Infra.Zero.Uc.Deployment.ClientSoftwareUpdateStrategy, CommonComponents.Infra.Zero.Uc ");
+            Console.WriteLine("*** Type 2 ***");
+
+            var t3 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            Console.WriteLine("*** Type 3 ***");
+
+            var t4 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, Culture=neutral");
+            Console.WriteLine("*** Type 4 ***");
+
+            var t5 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, Culture=neutral ");
+            Console.WriteLine("*** Type 5 ***");
+
+            var t6 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0");
+            Console.WriteLine("*** Type 6 ***");
+
+            var t7 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0 ");
+            Console.WriteLine("*** Type 7 ***");
+
+            var t8 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, PublicKeyToken=b77a5c561934e089");
+            Console.WriteLine("*** Type 8 ***");
+
+            var t9 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, PublicKeyToken=b77a5c561934e089 ");
+            Console.WriteLine("*** Type 9 ***");
+
+            var t10 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Culture=neutral");
+            Console.WriteLine("*** Type 10 ***");
+
+            var t11 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Culture=neutral ");
+            Console.WriteLine("*** Type 11 ***");
+
+            var t12 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Culture=neutral2");
+            Console.WriteLine("*** Type 12 ***");
+
+            var t13 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Version=4.0.0.0, PublicKeyToke2n=b77a5c561934e089");
+            Console.WriteLine("*** Type 13 ***");
+
+            var t14 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, Vedrsion=4.0.0.0, PublicKeyToke2n=b77a5c561934e089");
+            Console.WriteLine("*** Type 14 ***");
+
+            var t15 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection, System.ServiceModel2, PublicKeyToken=b77a5c561934e089, Version=4.0.0.0, Culture=neutral");
+            Console.WriteLine("*** Type 15 ***");
+
+            var t16 = Type.GetType("System.ServiceModel.Configuration.DiagnosticSection    , System.ServiceModel2, PublicKeyToken=b77a5c561934e089, Version=4.0.0.0, Culture=neutral    ");
+            Console.WriteLine("*** Type 16 ***");
+
 
         }
 
         private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args)
         {
-            Console.WriteLine($"Неудачно искали для загрузки :(args.Name) : {args.Name}");
-
-            // Using Mono - args.Name="CommonComponents.Infra.Zero.Uc , Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"
-            // It is wrong because using .Net Framework we can take args.Name="CommonComponents.Infra.Zero.Uc"
-            // This problem have been detected when I try use Mono with one application that use compare this parametr
-
-            // This problem with  mono_stringify_assembly_name in assembly.c
-
-            if (args.Name.Equals("CommonComponents.Infra.Zero.Uc"))
-                Console.WriteLine("args.Name is equal \"CommonComponents.Infra.Zero.Uc\"");
-
+            Console.WriteLine($" Resolving assembly name: {args.Name}");
             return null;
         }
     }
